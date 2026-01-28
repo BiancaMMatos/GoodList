764f8e1e64030f40e1a7ed7dca7c94b8078232b2
Bianca Maciel <61274369+BiancaMMatos@users.noreply.github.com>
2026-01-28 19:41:44 -0300

    Changing name

--- GoodList/GoodList/ListView.swift
+++ GoodList/GoodList/ListView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  ListViewView.swift
 //  GoodList
 //
 //  Created by Bianca Maciel on 28/01/26.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ListView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ListView()
 }
