47c469f904df2c6c99fce7ce938f537d81401b5b
Bianca Maciel <61274369+BiancaMMatos@users.noreply.github.com>
2026-01-29 12:55:33 -0300

    Adding UseCase for VM

--- GoodList/GoodList/Presentation/ViewModel/TaskListViewModel.swift
+++ GoodList/GoodList/Presentation/ViewModel/TaskListViewModel.swift
@@ -6,18 +6,33 @@
 //
 
 import Combine
+import Foundation
 
 final class ToDoTaskListViewModel: ObservableObject {
 
     @Published private(set) var tasks: [ToDoTask] = []
 
     private let fetchTasksUseCase: FetchToDoTasksUseCase
+    private let deleteTaskUseCase: DeleteToDoTaskUseCase
 
-    init(fetchTasksUseCase: FetchToDoTasksUseCase) {
+    init(
+        fetchTasksUseCase: FetchToDoTasksUseCase,
+        deleteTaskUseCase: DeleteToDoTaskUseCase
+    ) {
         self.fetchTasksUseCase = fetchTasksUseCase
+        self.deleteTaskUseCase = deleteTaskUseCase
     }
 
     func loadTasks() {
         tasks = fetchTasksUseCase.execute()
+    }
+
+    func delete(at offsets: IndexSet) {
+        offsets.forEach { index in
+            let task = tasks[index]
+            deleteTaskUseCase.execute(task: task)
+        }
+        
+        loadTasks()
     }
 }
